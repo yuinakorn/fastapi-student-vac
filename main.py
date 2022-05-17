8f25649c19f4daf6655818d7e0582ed876a2bd08
Yui <yuinakorn@hotmail.com>
2022-05-17 16:09:48 +0700

    complete3

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from fastapi import FastAPI
 from routes.student import student_router
 from routes.token import token_router
 
-app = FastAPI()
+description = "COVID-19 Immunization Data [RMUTL's Student]"
+
+app = FastAPI(
+    title="CMPHO API",
+    description=description,
+    version="1.0",
+)
 
 app.include_router(token_router.router)
 app.include_router(student_router.router)
